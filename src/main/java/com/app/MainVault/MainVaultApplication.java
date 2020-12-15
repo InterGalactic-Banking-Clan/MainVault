64191246279256de8bb00c54eae24df2b980eb03
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-15 13:07:47 -0800

    added the spring package, woops

---
+++ src/main/java/com/app/MainVault/MainVaultApplication.java
@@ -0,0 +1,13 @@
+package com.app.MainVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainVaultApplication.class, args);
+	}
+
+}
