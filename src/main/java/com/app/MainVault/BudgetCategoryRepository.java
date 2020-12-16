f0021cf14d766209d0a7f24b3242d8c823d7454c
Alden Davidson <adavidson+github@protonmail.ch>
2020-12-16 13:09:03 -0800

    added basic repos for the three tables/classes

---
+++ src/main/java/com/app/MainVault/BudgetCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.app.MainVault;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BudgetCategoryRepository extends CrudRepository<BudgetCategory, Integer> {
+}
