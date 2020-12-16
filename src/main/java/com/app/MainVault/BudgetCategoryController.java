d4014cbc221ce5cd5d5788201de147b0dd1a8034
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-16 14:16:24 -0800

    Attempted to create some endpoints for Budget Category
    Added POST anf PATCH mapping

--- src/main/java/com/app/MainVault/BudgetCategoryController.java
+++ src/main/java/com/app/MainVault/BudgetCategoryController.java
@@ -1,18 +1,30 @@
 package com.app.MainVault;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BudgetCategoryController {
-    @GetMapping("/income")
-    public String listOfIncomeSources() {
-        return "This GET request is intended to allow the user to list out all income sources";
-    }
+//    @GetMapping("/income")
+//    public String listOfIncomeSources() {
+//        return "This GET request is intended to allow the user to list out all income sources";
+//    } Commented out in case we need later
 
     @GetMapping("/budget")
     public String listOfBudgetCategories() {
         return "This GET request is intended to allow the user to customize their budget categories";
+    }
+
+    @PostMapping("/budget/categories")
+    public String customizingCategories() {
+        return "This POST request allows the user to customize their budget categories";
+    }
+
+    @PatchMapping("/budget/categories")
+    public String modifyingCustomCategories() {
+        return "This PATCH request allows the user to modify/edit their custom categories";
     }
 
 }
