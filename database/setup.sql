ad709571a3e2afb1afda5edab48c7df196498e66
Tom Thorpe <tom53086@gmail.com>
2020-12-16 12:11:26 -0800

    added application.properties

--- database/setup.sql
+++ database/setup.sql
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-=======
 # setup.sql
->>>>>>> origin/main
+
 CREATE DATABASE IF NOT EXISTS `main_vault`;
 CREATE DATABASE IF NOT EXISTS `main_vault_test`;
 
 CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'local';
-<<<<<<< HEAD
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-=======
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
->>>>>>> origin/main
