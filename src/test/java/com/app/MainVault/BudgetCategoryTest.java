61947ab003000b5cf177532b7fed743be44b08e7
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-18 07:56:12 -0800

    Sharon branch (#21)
    * added my name and dev header
    
    * added fancy title
    
    * initial commit
    
    * added docker-compose for a MySQL database
    
    * possibly fixed the database volume bologna
    
    * moved mysql to 3309 to avoid local installs
    
    * added foreign key definitions for userID
    
    * Added the application.properties for test
    
    * removed hanging @Test annotations to allow other tests to run
    
    * added import controller with basic tests
    
    * added test data for importing
    
    * Attempted to create some endpoints for Budget Category
    
    * Attempted to create some endpoints for Budget Category
    
    Added POST anf PATCH mapping
    
    * Attempted to create some endpoints for Budget Category
    
    Added POST anf PATCH mapping
    
    Changed POST and GET mapping; added PUT and DELETE mapping; removed POST mapping
    
    * Changed line 14 from crud to budget; commented out line 17;changed long to int in lines 32 and 53
    
    * Changed line 14 from crud to budget; commented out line 17;changed long to int in lines 32 and 53
    
    Added a line to example why using ResponseEntity<>
    
    * Changed line 14 from crud to budget; commented out line 17;changed long to int in lines 32 and 53
    
    Added a line to example why using ResponseEntity<>
    
    Created tests for endpoints
    
    * Changed line 14 from crud to budget; commented out line 17;changed long to int in lines 32 and 53
    
    Added a line to example why using ResponseEntity<>
    
    Created tests for endpoints
    
    AD helped me get my tests cleaned up
    
    * Group helped me fix the delete issue; Thx guys!
    
    * Changed url string on line 104 to an object
    
    * Attempted to include a .content line; didn't take but the rest of the code remained the same
    
    Co-authored-by: Alden Davidson <adavidson+github@protonmail.ch>
    Co-authored-by: “adaehler” <antonio_daehler@yahoo.com>
    Co-authored-by: Graham McLaughlin <grahammclaughlin1324@gmail.com>

--- src/test/java/com/app/MainVault/BudgetCategoryTest.java
+++ src/test/java/com/app/MainVault/BudgetCategoryTest.java
@@ -1,8 +1,112 @@
 package com.app.MainVault;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BudgetCategoryTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    //Lines 23-24 is pulling the sampling data to run test against
+    @Autowired
+    private BudgetCategoryRepository budgetCategoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @Transactional
+    public void testingBudgetStatus() throws Exception {
+        this.mvc.perform(
+                get("/budget")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }//Checks the status to the server
+
+    @Test
+    @Transactional
+    public void gettingBudgetCatById() throws Exception {
+        BudgetCategory budgetCategory = new BudgetCategory();
+        User user = new User();
+        user.setId(1);
+        budgetCategory.setUser(user);
+        budgetCategoryRepository.save(budgetCategory);
+
+        MockHttpServletRequestBuilder request = get("/budget/1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)));
+    }//Testing the ability to get a budget category by it's id
+
+    @Test
+    @Transactional
+    public void creatingNewBudgetCategory() throws Exception {
+
+        MockHttpServletRequestBuilder request = post("/budget/categories/Car Loan")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is("Car Loan")));
+
+    }//Testing the ability to create a budget category; getting status errors - Expected status 201, Actual status 405
+
+    @Test
+    @Transactional
+    public void updatingBudgetCategory() throws Exception {
+
+        MockHttpServletRequestBuilder request = put("/budget/2")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.monthlyAllocation", is(100)));
+    }//Testing the ability to modify/edit a budget category; status errors - expected status 202, actual status 400
+
+    @Test
+    @Transactional
+    public void deletingBudgetCategory() throws Exception {
+        BudgetCategory budgetCategory = new BudgetCategory();
+        User user = new User();
+        user.setId(6);
+        budgetCategory.setUser(user);
+        BudgetCategory result = budgetCategoryRepository.save(budgetCategory);
+        String url = "/budget/" + result.getId();
+
+        MockHttpServletRequestBuilder request = delete(url)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk());
+
+        MockHttpServletRequestBuilder request2 = get(url)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request2)
+                .andExpect(status().isNotFound());
+    }//Testing the ability to delete a budget category; request processing failed
 
 }
